32be6788e002a0630e2e31416e4682f04edf6b67
BrandonHo <hoyuenling93@gmail.com>
2020-04-04 02:16:58 +0200

    Updates

---
+++ precache-manifest.2edea4f535526c10793c7329368c50a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75d50d31ee4bc606f653925ffed24163",
+    "url": "/BudgetDashboard/index.html"
+  },
+  {
+    "revision": "d4ffab4c0d864930d0bc",
+    "url": "/BudgetDashboard/static/css/2.b9b2dd63.chunk.css"
+  },
+  {
+    "revision": "3d4174e929ad4b13ba6a",
+    "url": "/BudgetDashboard/static/css/main.0b8c7dcc.chunk.css"
+  },
+  {
+    "revision": "d4ffab4c0d864930d0bc",
+    "url": "/BudgetDashboard/static/js/2.137c6361.chunk.js"
+  },
+  {
+    "revision": "06af5da2b5db73bc7f2124291a828d61",
+    "url": "/BudgetDashboard/static/js/2.137c6361.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d4174e929ad4b13ba6a",
+    "url": "/BudgetDashboard/static/js/main.32f00724.chunk.js"
+  },
+  {
+    "revision": "1699f9e97869f2fd3a94",
+    "url": "/BudgetDashboard/static/js/runtime-main.817d87db.js"
+  }
+]);
